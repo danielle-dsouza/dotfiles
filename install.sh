4b62065fdea66eb075edde2d34cef33b6fb8a840
Danielle DSouza <danielle.dsouza@shopify.com>
2021-06-28 16:35:26 -0400

    Remove orders command

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-sudo apt-get -y install nano
 if [[ "$SPIN" = 1 ]]
 then
   if [[ "$SPIN_REPO_SOURCE_PATH" = "/src/github.com/shopify/shopify" ]]
   then
-    rake dev:orders:generate_dummy_orders NUM=10
+    sudo apt-get -y install nano
+    # rake dev:orders:generate_dummy_orders
   fi
 fi
